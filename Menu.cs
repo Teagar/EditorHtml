7cb153cc57d862d760ca0b4ce4a345596540deae
Teagar <cyanofc@gmail.com>
2024-01-24 20:15:29 -0300

    Add options

--- Menu.cs
+++ Menu.cs
@@ -18,7 +18,10 @@ namespace EditorHtml
     public static void DrawScreen()
     {
       
-      BuildCanvas(30, 15);
+      BuildCanvas(30, 12);
+      WriteOptions();
+      short option = 0;
+      Int16.TryParse(Console.ReadLine(), out option);
     }
 
     public static void BuildCanvas(int x, int y, char separator = '+', char line = '-', char column = '|')
@@ -52,6 +55,29 @@ namespace EditorHtml
       BuildX();
       BuildY();
       BuildX();
+    }
+
+    public static void WriteOptions()
+    {
+
+      Console.SetCursorPosition(10, 2);
+      Console.Write("Editor HTML");
+      Console.SetCursorPosition(6, 3);
+      Console.Write("-------------------");
+
+      Console.SetCursorPosition(3, 5);
+      Console.Write("1 -\tOpen");
+      Console.SetCursorPosition(3, 6);
+      Console.Write("2 -\tEdit");
+      Console.SetCursorPosition(3, 7);
+      Console.Write("3 -\tWrite");
+      Console.SetCursorPosition(3, 8);
+      Console.Write("0 -\tExit");
+      Console.SetCursorPosition(3, 9);
+      Console.Write("--------------------------");
+
+      Console.SetCursorPosition(3, 11);
+      Console.Write("Option: ");
     }
 
   }
